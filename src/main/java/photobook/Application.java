fc12e15dde5548593e7f66b2224102b812b3137f
cezarf2 <cezarfilho_bx@hotmail.com>
2018-12-03 15:20:36 -0300

    Inserindo serialização no model Usuario

--- src/main/java/photobook/Application.java
+++ src/main/java/photobook/Application.java
@@ -1,9 +1,11 @@
 package photobook;
 
+import org.apache.catalina.core.ApplicationContext;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 @SpringBootApplication
 public class Application extends SpringBootServletInitializer{
--- src/main/java/photobook/model/Usuario.java
+++ src/main/java/photobook/model/Usuario.java
@@ -1,5 +1,7 @@
 package photobook.model;
 
+import java.io.Serializable;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -10,7 +12,12 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 @Entity(name="usuario")
-public class Usuario{
+public class Usuario implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
