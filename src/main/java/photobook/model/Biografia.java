6f9fc37afee0622402edb3cfc2ed0d9a746c5063
cezarf2 <cezarfilho_bx@hotmail.com>
2018-12-03 15:26:25 -0300

    Inserindo serialização na Biografia

--- src/main/java/photobook/model/Biografia.java
+++ src/main/java/photobook/model/Biografia.java
@@ -1,5 +1,7 @@
 package photobook.model;
 
+import java.io.Serializable;
+
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -12,7 +14,11 @@ import javax.validation.constraints.Size;
 
 
 @Entity(name="biografia")
-public class Biografia{
+public class Biografia implements Serializable{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
